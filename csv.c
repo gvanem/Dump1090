77accfcfed393ddc304165a1c384b22ee8125a67
Gisle Vanem <gisle.vanem@gmail.com>
2021-04-28 17:08:38 +0200

    Add files for CSV parsing.

---
+++ csv.c
@@ -0,0 +1,298 @@
+/**\file    csv.c
+ * \ingroup Misc
+ *
+ * \brief Implements a generic parser for CSV files.
+ *
+ * The parsing is loosely adapting the rules in: https://tools.ietf.org/html/rfc4180
+ */
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <limits.h>
+#include <assert.h>
+#include <ctype.h>
+#include <errno.h>
+#include "csv.h"
+
+#define DEFAULT_BUF_SIZE 1000
+
+#define PUTC(c)  do {                                                    \
+                   if (ctx->parse_ptr < ctx->parse_buf + ctx->line_size) \
+                      *ctx->parse_ptr++ = c;                             \
+                 } while (0)
+
+/**
+ * A simple state-machine for parsing CSV records.
+ *
+ * The parser starts in this state.
+ */
+static void state_normal (struct CSV_context *ctx)
+{
+  if (ctx->c_in == ctx->delimiter)
+  {
+    ctx->state = STATE_STOP;
+    return;
+  }
+  switch (ctx->c_in)
+  {
+    case -1:
+         ctx->state = STATE_EOF;
+         break;
+    case '"':
+         ctx->state = STATE_QUOTED;
+         break;
+    case '\r':     /* ignore */
+         break;
+    case '\n':
+         if (ctx->field_num > 0)     /* If field == 0, ignore empty lines */
+              ctx->state = STATE_STOP;
+         else ctx->empty_lines++;
+         break;
+    case '#':
+         if (ctx->field_num == 0)    /* If field == 0, ignore comment lines */
+         {
+           ctx->state = STATE_COMMENT;
+           ctx->comment_lines++;
+         }
+         else
+           PUTC (ctx->c_in);
+         break;
+    default:
+         PUTC (ctx->c_in);
+         break;
+  }
+}
+
+/**
+ * If the parser find a quote (`"`) in `state_normal()`, it enters this state
+ * to find the end of the quote. Ignoring escaped quotes (i.e. a `\\"`).
+ */
+static void state_quoted (struct CSV_context *ctx)
+{
+  switch (ctx->c_in)
+  {
+    case -1:
+         ctx->state = STATE_EOF;
+         break;
+    case '"':
+         ctx->state = STATE_NORMAL;
+         break;
+    case '\r':     /* ignore, but should not occur since `fopen (file, "rt")` was used */
+         break;
+    case '\n':     /* add a space in this field */
+         PUTC (' ');
+         break;
+    case '\\':
+         ctx->state = STATE_ESCAPED;
+         break;
+    default:
+         PUTC (ctx->c_in);
+         break;
+  }
+}
+
+/**
+ * Look for an escaped quote. <br>
+ * Go back to `state_quoted()` when found.
+ */
+static void state_escaped (struct CSV_context *ctx)
+{
+  switch (ctx->c_in)
+  {
+    case -1:
+         ctx->state = STATE_EOF;
+         break;
+    case '"':       /* '\"' -> '"' */
+         PUTC ('"');
+         ctx->state = STATE_QUOTED;
+         break;
+    case '\r':
+    case '\n':
+         break;
+    default:
+         ctx->state = STATE_QUOTED; /* Unsupported ctrl-char. Go back */
+         break;
+  }
+}
+
+/**
+ * Do nothing until a newline. <br>
+ * Go back to `state_normal()` when found.
+ */
+static void state_comment (struct CSV_context *ctx)
+{
+  switch (ctx->c_in)
+  {
+    case -1:
+         ctx->state = STATE_EOF;
+         break;
+    case '\n':
+         ctx->state = STATE_NORMAL;
+         break;
+  }
+}
+
+static void state_illegal (struct CSV_context *ctx)
+{
+  ctx->state = STATE_EOF;
+}
+
+/**
+ * Read from `ctx->file` until end-of-field.
+ */
+static const char *CSV_get_next_field (struct CSV_context *ctx)
+{
+  char     *ret;
+  CSV_STATE new_state = STATE_ILLEGAL;
+  CSV_STATE old_state = STATE_ILLEGAL;
+
+  ctx->parse_ptr = ctx->parse_buf;
+
+  while (1)
+  {
+    ctx->c_in = fgetc (ctx->file);
+
+    old_state = ctx->state;  (*ctx->state_func) (ctx);
+    new_state = ctx->state;
+
+    /* Set new state for this context. (Or stay in same state).
+     */
+    switch (new_state)
+    {
+      case STATE_NORMAL:
+           ctx->state_func = state_normal;
+           break;
+      case STATE_QUOTED:
+           ctx->state_func = state_quoted;
+           break;
+      case STATE_ESCAPED:
+           ctx->state_func = state_escaped;
+           break;
+      case STATE_COMMENT:
+           ctx->state_func = state_comment;
+           break;
+      case STATE_ILLEGAL:   /* Avoid compiler warning */
+      case STATE_STOP:
+      case STATE_EOF:
+           break;
+    }
+    if (new_state == STATE_STOP && ctx->delimiter == ' ')
+    {
+      while ((ctx->c_in = fgetc (ctx->file)) == ' ') ;
+      ungetc (ctx->c_in, ctx->file);
+    }
+
+    if (new_state == STATE_STOP || new_state == STATE_EOF)
+       break;
+  }
+
+  *ctx->parse_ptr = '\0';
+  ret = ctx->parse_buf;
+  if (new_state == STATE_EOF)
+     return (NULL);
+  return (ret);
+}
+
+/**
+ * Open and parse CSV file and extract one record by calling the callback for each found field.
+ *
+ * \param[in]  ctx  the CSV context to work with.
+ * \retval     the number of CSV-records that could be parsed.
+ */
+static int CSV_parse_file (struct CSV_context *ctx)
+{
+  for (ctx->field_num = 0; ctx->field_num < ctx->num_fields; ctx->field_num++)
+  {
+    const char *val;
+    int         rc;
+
+    ctx->state = STATE_NORMAL;
+    ctx->state_func = state_normal;
+
+    val = CSV_get_next_field (ctx);
+    if (!val)
+       goto quit;
+
+    rc = (*ctx->callback) (ctx, val);
+    if (!rc)
+       break;
+  }
+
+  ctx->rec_num++;
+  return (ctx->field_num == ctx->num_fields);
+
+quit:
+  if (ctx->state == STATE_EOF)
+     return (0);
+
+  ctx->parse_errors++;
+  return (0);
+}
+
+/**
+ * Check for unset members of the CSV-context. <br>
+ * Set the field-delimiter to `,` if not already done.
+ *
+ * \param[in]  ctx  the CSV context to work with.
+ * \retval     1 if the members are okay.
+ */
+static int CSV_check_and_fill_ctx (struct CSV_context *ctx)
+{
+  if (ctx->num_fields == 0)
+     return (0);
+
+  if (!ctx->delimiter)
+     ctx->delimiter = ',';
+
+  if (strchr("#\"\r\n", ctx->delimiter))
+    return (0);
+
+  if (ctx->rec_max == 0)
+     ctx->rec_max = UINT_MAX;
+
+  if (!ctx->callback)
+     return (0);
+
+  if (!ctx->file_name)
+     return (0);
+
+  if (ctx->line_size == 0)
+     ctx->line_size = DEFAULT_BUF_SIZE;
+
+  ctx->parse_buf = malloc (ctx->line_size+1);
+  if (!ctx->parse_buf)
+     return (0);
+
+  ctx->file = fopen (ctx->file_name, "rt");
+  if (!ctx->file)
+  {
+    free (ctx->parse_buf);
+    return (0);
+  }
+
+  setvbuf (ctx->file, NULL, _IOFBF, 2*ctx->line_size);
+  ctx->state_func = state_illegal;
+  ctx->state      = STATE_ILLEGAL;
+  ctx->rec_num    = 0;
+  return (1);
+}
+
+/**
+ * Open and parse a CSV-file.
+ */
+unsigned CSV_open_and_parse_file (struct CSV_context *ctx)
+{
+  if (!CSV_check_and_fill_ctx(ctx))
+     return (0);
+
+  while (1)
+  {
+    if (!CSV_parse_file(ctx) || ctx->rec_num >= ctx->rec_max)
+       break;
+  }
+  fclose (ctx->file);
+  ctx->file = NULL;
+  free (ctx->parse_buf);
+  return (ctx->rec_num);
+}
+
---
+++ csv.h
@@ -0,0 +1,52 @@
+/**\file    csv.h
+ * \ingroup Misc
+ */
+#ifndef _CSV_H
+#define _CSV_H
+
+/**
+ * \enum The CSV-parser states
+ */
+typedef enum CSV_STATE {
+        STATE_ILLEGAL = 0,
+        STATE_NORMAL,
+        STATE_QUOTED,
+        STATE_ESCAPED,
+        STATE_COMMENT,
+        STATE_STOP,
+        STATE_EOF,
+      } CSV_STATE;
+
+struct CSV_context;
+
+/**
+ * \typedef The CSV-parser state-functions matches this:
+ */
+typedef void (*csv_state_t) (struct CSV_context *ctx);
+
+/**
+ * \typedef CSV_context
+ * Keep all data used for CSV parsing in this context.
+ */
+typedef struct CSV_context {
+        const char *file_name;
+        FILE       *file;
+        unsigned    field_num;
+        unsigned    num_fields;
+        int         delimiter;
+        int       (*callback) (struct CSV_context *ctx, const char *value);
+        unsigned    rec_num;
+        unsigned    rec_max;
+        unsigned    line_size;
+        unsigned    comment_lines;
+        unsigned    empty_lines;
+        unsigned    parse_errors;
+        char       *parse_buf, *parse_ptr;
+        csv_state_t state_func;
+        CSV_STATE   state;
+        int         c_in;
+      } CSV_context;
+
+unsigned CSV_open_and_parse_file (struct CSV_context *ctx);
+
+#endif /* _CSV_H */
