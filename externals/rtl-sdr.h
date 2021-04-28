16f695d8efcefb5308b34fbfc2cf28c336fdc929
Gisle Vanem <gisle.vanem@gmail.com>
2021-04-28 10:41:58 +0200

    Use the improved 'rtlsdr' library from:   https://github.com/old-dab/libusb

--- externals/rtl-sdr.h
+++ externals/rtl-sdr.h
@@ -24,7 +24,13 @@
 extern "C" {
 #endif
 
+#ifdef _WIN32
+#define usleep(x) Sleep(x/1000)
+#endif
+
+
 #include <stdint.h>
+#include <stddef.h>
 #include <rtl-sdr_export.h>
 
 typedef struct rtlsdr_dev rtlsdr_dev_t;
@@ -142,6 +148,13 @@ RTLSDR_API int rtlsdr_write_eeprom(rtlsdr_dev_t *dev, uint8_t *data,
 RTLSDR_API int rtlsdr_read_eeprom(rtlsdr_dev_t *dev, uint8_t *data,
 				  uint8_t offset, uint16_t len);
 
+/*!
+ * Set the frequency the device is tuned to.
+ *
+ * \param dev the device handle given by rtlsdr_open()
+ * \param frequency in Hz
+ * \return 0 on error, frequency in Hz otherwise
+ */
 RTLSDR_API int rtlsdr_set_center_freq(rtlsdr_dev_t *dev, uint32_t freq);
 
 /*!
@@ -178,6 +191,27 @@ enum rtlsdr_tuner {
 	RTLSDR_TUNER_R820T,
 	RTLSDR_TUNER_R828D
 };
+
+enum rtlsdr_demod {
+	SLAVE_DEMOD_NONE = 0,
+	SLAVE_DEMOD_MN88472,
+	SLAVE_DEMOD_MN88473,
+	SLAVE_DEMOD_SI2168,
+	SLAVE_DEMOD_CXD2837ER
+};
+
+#define MN8847X_I2C_ADDR	0x38
+#define MN8847X_CHECK_ADDR	0xff
+#define MN88472_CHIP_ID		0x02
+#define MN88473_CHIP_ID		0x03
+
+#define SI2168_I2C_ADDR		0xc8
+#define SI2168_CHECK_ADDR	0x00
+#define SI2168_CHIP_ID		0x80
+
+#define CXD2837_I2C_ADDR	0xd8
+#define CXD2837_CHECK_ADDR	0xfd
+#define CXD2837ER_CHIP_ID	0xb1
 
 /*!
  * Get the tuner type.
@@ -220,9 +254,22 @@ RTLSDR_API int rtlsdr_set_tuner_gain(rtlsdr_dev_t *dev, int gain);
  *
  * \param dev the device handle given by rtlsdr_open()
  * \param bw bandwidth in Hz. Zero means automatic BW selection.
+ * \param applied_bw is applied bandwidth in Hz, or 0 if unknown
+ * \param apply_bw: 1 to really apply configure the tuner chip; 0 for just returning applied_bw
  * \return 0 on success
  */
-RTLSDR_API int rtlsdr_set_tuner_bandwidth(rtlsdr_dev_t *dev, uint32_t bw);
+RTLSDR_API int rtlsdr_set_and_get_tuner_bandwidth(rtlsdr_dev_t *dev, uint32_t bw, uint32_t *applied_bw, int apply_bw );
+
+RTLSDR_API int rtlsdr_set_tuner_bandwidth(rtlsdr_dev_t *dev, uint32_t bw );
+
+/*!
+ * Set the mixer sideband for the device.
+ *
+ * \param dev the device handle given by rtlsdr_open()
+ * \param sideband mixer sideband 0 means lower sideband, 1 means upper sideband.
+ * \return 0 on success
+ */
+RTLSDR_API int rtlsdr_set_tuner_sideband(rtlsdr_dev_t *dev, int sideband);
 
 /*!
  * Get actual gain the device is configured to.
@@ -231,6 +278,7 @@ RTLSDR_API int rtlsdr_set_tuner_bandwidth(rtlsdr_dev_t *dev, uint32_t bw);
  * \return 0 on error, gain in tenths of a dB, 115 means 11.5 dB.
  */
 RTLSDR_API int rtlsdr_get_tuner_gain(rtlsdr_dev_t *dev);
+
 
 /*!
  * Set the intermediate frequency gain for the device.
@@ -313,6 +361,25 @@ RTLSDR_API int rtlsdr_set_direct_sampling(rtlsdr_dev_t *dev, int on);
  */
 RTLSDR_API int rtlsdr_get_direct_sampling(rtlsdr_dev_t *dev);
 
+enum rtlsdr_ds_mode {
+	RTLSDR_DS_IQ = 0,	/* I/Q quadrature sampling of tuner output */
+	RTLSDR_DS_I,		/* 1: direct sampling on I branch: usually not connected */
+	RTLSDR_DS_Q,		/* 2: direct sampling on Q branch: HF on rtl-sdr v3 dongle */
+	RTLSDR_DS_I_BELOW,	/* 3: direct sampling on I branch when frequency below 'DS threshold frequency' */
+	RTLSDR_DS_Q_BELOW	/* 4: direct sampling on Q branch when frequency below 'DS threshold frequency' */
+};
+
+/*!
+ * Set direct sampling mode with threshold
+ *
+ * \param dev the device handle given by rtlsdr_open()
+ * \param mode static modes 0 .. 2 as in rtlsdr_set_direct_sampling(). other modes do automatic switching
+ * \param freq_threshold direct sampling is used below this frequency, else quadrature mode through tuner
+ *   set 0 for using default setting per tuner - not fully implemented yet!
+ * \return negative on error, 0 on success
+ */
+RTLSDR_API int rtlsdr_set_ds_mode(rtlsdr_dev_t *dev, enum rtlsdr_ds_mode mode, uint32_t freq_threshold);
+
 /*!
  * Enable or disable offset tuning for zero-IF tuners, which allows to avoid
  * problems caused by the DC offset of the ADCs and 1/f noise.
@@ -343,19 +410,6 @@ typedef void(*rtlsdr_read_async_cb_t)(unsigned char *buf, uint32_t len, void *ct
  * Read samples from the device asynchronously. This function will block until
  * it is being canceled using rtlsdr_cancel_async()
  *
- * NOTE: This function is deprecated and is subject for removal.
- *
- * \param dev the device handle given by rtlsdr_open()
- * \param cb callback function to return received samples
- * \param ctx user specific context to pass via the callback function
- * \return 0 on success
- */
-RTLSDR_API int rtlsdr_wait_async(rtlsdr_dev_t *dev, rtlsdr_read_async_cb_t cb, void *ctx);
-
-/*!
- * Read samples from the device asynchronously. This function will block until
- * it is being canceled using rtlsdr_cancel_async()
- *
  * \param dev the device handle given by rtlsdr_open()
  * \param cb callback function to return received samples
  * \param ctx user specific context to pass via the callback function
@@ -381,7 +435,20 @@ RTLSDR_API int rtlsdr_read_async(rtlsdr_dev_t *dev,
 RTLSDR_API int rtlsdr_cancel_async(rtlsdr_dev_t *dev);
 
 /*!
- * Enable or disable the bias tee on GPIO PIN 0.
+ * Read from the remote control (RC) infrared (IR) sensor
+ *
+ * \param dev the device handle given by rtlsdr_open()
+ * \param buf buffer to write IR signal (MSB=pulse/space, 7LSB=duration*20usec), recommended 128-bytes
+ * \param buf_len size of buf
+ * \return 0 if no signal, >0 number of bytes written into buf, <0 for error
+ */
+RTLSDR_API int rtlsdr_ir_query(rtlsdr_dev_t *dev, uint8_t *buf, size_t buf_len);
+
+void rtlsdr_set_gpio_bit(rtlsdr_dev_t *dev, uint8_t gpio, int val);
+
+/*!
+ * Enable or disable the bias tee on GPIO PIN 0. (Works for rtl-sdr.com v3 dongles)
+ * See: http://www.rtl-sdr.com/rtl-sdr-blog-v-3-dongles-user-guide/
  *
  * \param dev the device handle given by rtlsdr_open()
  * \param on  1 for Bias T on. 0 for Bias T off.
@@ -399,6 +466,67 @@ RTLSDR_API int rtlsdr_set_bias_tee(rtlsdr_dev_t *dev, int on);
  */
 RTLSDR_API int rtlsdr_set_bias_tee_gpio(rtlsdr_dev_t *dev, int gpio, int on);
 
+
+/*!
+ * Sets multiple options from a string encoded like "bw=300:agc=0:gain=27.3:dagc=0:T=1".
+ * this is a helper function, that programs don't need to implement every single option
+ *   at the command line interface.
+ * Options are seperated by colon ':'.
+ * There mustn't be extra spaces between option name and '='.
+ * option 'f' set center frequency as in rtlsdr_set_center_freq()
+ * option 'bw' sets tuner bandwidth as in rtlsdr_set_tuner_bandwidth()
+ *   - but value is in kHz.
+ * option 'agc' sets tuner gain mode as with rtlsdr_set_tuner_gain_mode():
+ *   '1' means manual gain mode shall be enabled.
+ * option 'gain' sets tuner gain as with rtlsdr_set_tuner_gain():
+ *   values in tenth dB.
+ * option 'dagc' or 'dgc' de/activates digital agc as with rtlsdr_set_agc_mode().
+ *   value 1 to enable. 0 to disable.
+ * option 'ds' set direct sampling as with rtlsdr_set_direct_sampling():
+ *   '0' to deactivate, '1' or 'i' for I-ADC input, '2' or 'q' for Q-ADC input
+ * option 't' or 'T' for enabling bias tee on GPIO PIN 0 as with rtlsdr_set_bias_tee():
+ *   '1' for Bias T on. '0' for Bias T off.
+ *
+ * \param dev the device handle given by rtlsdr_open()
+ * \param opts described option string
+ * \param verbose print parsed options to stderr
+ */
+RTLSDR_API int rtlsdr_set_opt_string(rtlsdr_dev_t *dev, const char *opts, int verbose);
+
+
+/*!
+ * Exposes/permits hacking of Tuner-specific I2C registers
+ *
+ * \param dev           the device handle given by rtlsdr_open()
+ * \param i2c_register  1 for Bias T on. 0 for Bias T off.
+ * \param mask          8-bit bitmask, indicating which bits shall be set
+ * \param data          8-bit data, which shall be set
+ * \return -1 if device is not initialized. 0 otherwise.
+ */
+RTLSDR_API int rtlsdr_set_tuner_i2c_register(rtlsdr_dev_t *dev, unsigned i2c_register, unsigned mask, unsigned data);
+
+RTLSDR_API int rtlsdr_get_tuner_i2c_register(rtlsdr_dev_t *dev, unsigned char* data, int *len, int *strength);
+
+RTLSDR_API void rtlsdr_cal_imr(const int cal_imr);
+
+RTLSDR_API int rtlsdr_reset_demod(rtlsdr_dev_t *dev);
+
+/*!
+ * Enable or disable frequency dithering for r820t tuners.
+ * Must be performed before freq_set().
+ * Fails for other tuners.
+ *
+ * \param dev the device handle given by rtlsdr_open()
+ * \param on 0 means disabled, 1 enabled
+ * \return 0 on success
+ */
+RTLSDR_API int rtlsdr_set_dithering(rtlsdr_dev_t *dev, int dither);
+
+#ifdef DEBUG
+RTLSDR_API void print_demod_register(rtlsdr_dev_t *dev, uint8_t page);
+RTLSDR_API void print_usb_register(rtlsdr_dev_t *dev, uint16_t adr);
+RTLSDR_API int  rtlsdr_demod_write_reg(rtlsdr_dev_t *dev, uint8_t page, uint16_t addr, uint16_t val, uint8_t len);
+#endif
 
 #ifdef __cplusplus
 }
