59be55ca028d139ffaeb0060d55c07b13dd4c390
Gisle Vanem <gvanem@yahoo.no>
2022-02-07 13:08:26 +0100

    Add a 'version.h' from https://github.com/old-dab/rtlsdr/blob/master/src/version.h

---
+++ externals/rtl-sdr/version.h
@@ -0,0 +1,20 @@
+/* This file is parsed by m4 and windres and RC.EXE so please keep it simple. */
+
+#ifndef RTLSDR_MAJOR
+#define RTLSDR_MAJOR 0
+#endif
+#ifndef RTLSDR_MINOR
+#define RTLSDR_MINOR 9
+#endif
+#ifndef RTLSDR_MICRO
+#define RTLSDR_MICRO 6
+#endif
+#ifndef RTLSDR_NANO
+#define RTLSDR_NANO	4
+#endif
+
+/* RTLSDR__RC is the release candidate suffix. Should normally be empty. */
+#ifndef RTLSDR_RC
+#define RTLSDR_RC ""
+
+#endif
