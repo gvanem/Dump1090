4858ecdff16dab6466bab5bb5de77b0ace473703
Gisle Vanem <gvanem@yahoo.no>
2023-01-30 13:23:09 +0100

    Added a '--minify' compress option to 'tools/gen_packed_fs.py' using the 'csscompressor', 'htmlmin', jsmin' packages. Also write the original text as a comment in the hex-dump (unless option '--no-comments' was used).

--- tools/gen_packed_fs.py
+++ tools/gen_packed_fs.py
@@ -6,11 +6,19 @@ Inspired by Mongoose' 'test/pack.c' program.
 
 import os, sys, stat, time, fnmatch, argparse
 
-opt         = None
-total_bytes = 0
-files_dict  = dict()
-my_name     = os.path.basename (__file__)
-PY2         = (sys.version_info.major == 2)
+try:
+  import csscompressor, htmlmin, jsmin, io
+  have_minify = True
+except ImportError:
+  have_minify = False
+
+opt        = None
+files_dict = dict()
+my_name    = os.path.basename (__file__)
+PY2        = (sys.version_info.major == 2)
+
+total_in_bytes  = 0
+total_out_bytes = 0
 
 C_TOP = """//
 // Generated at %s by
@@ -23,6 +31,12 @@ C_TOP = """//
 int         mg_pack_case (int on);
 const char *mg_unlist (size_t i);
 const char *mg_unpack (const char *name, size_t *size, time_t *mtime);
+
+#if defined(__clang__) || defined(__GNUC__)
+  #pragma GCC diagnostic ignored "-Wcomment"  /* multi-line // comment [-Wcomment] */
+#elif defined(_MSC_VER)
+  #pragma warning (disable:4010) /* single-line comment contains line-continuation character */
+#endif
 
 """
 
@@ -111,20 +125,70 @@ def fmt_number (num):
      ret = "%d" % (num)
   return ret
 
-def generate_array (in_file, num, out):
-  trace (1, "Generating C-array for '%s'" % in_file)
+def dump_hex (in_file, out_file, data, data_len, len_in, num):
+  files_dict [in_file]["fsize"] = data_len
+  out_file.write ("//\n// Minified version generated from '%s' (%d%% saving) \n//\n" % (in_file, 100 - 100*data_len/len_in))
+  out_file.write ("static const unsigned char file%d[] = {\n" % num)
+  comment = [ " // ", "" ] [opt.nocomments]
+  for n in range(0, data_len):
+      c = data[n]
+      assert ord(c) <= 255
+      out_file.write (" 0x%02X," % ord(c))
+      if not opt.nocomments:
+         if c not in ['\r', '\n']:
+            comment += str(c)
+      if (n + 1) % 16 == 0:
+         out_file.write ("%s\n" % comment)
+         comment = [ " // ", "" ] [opt.nocomments]
+  out_file.write (" 0x00\n};\n\n")
+
+def generate_array_css (in_file, out_file, num):
+  with open (in_file, "r") as f:
+       data_in  = f.read(-1)
+       data_out = csscompressor.compress (data_in, preserve_exclamation_comments = False)
+       len_in   = len(data_in)
+       len_out  = len(data_out)
+       dump_hex (in_file, out_file, data_out, len_out, len_in, num)
+  return len_in, len_out
+
+def generate_array_html (in_file, out_file, num):
+  with open (in_file, "r") as f:
+       data_in  = f.read(-1)
+       data_out = htmlmin.minify (data_in, remove_comments = True, remove_empty_space = False)
+       len_in  = len(data_in)
+       len_out = len(data_out)
+       dump_hex (in_file, out_file, data_out, len_out, len_in, num)
+  return len_in, len_out
+
+def generate_array_js (in_file, out_file, num):
+  with open (in_file, "r") as f:
+       data_in = f.read(-1)
+       ins  = io.StringIO (data_in)
+       outs = io.StringIO()
+       jsmin.JavascriptMinify().minify (ins, outs)
+       data_out = outs.getvalue()
+       len_in   = len(data_in)
+       len_out  = len(data_out)
+       dump_hex (in_file, out_file, data_out, len_out, len_in, num)
+  return len_in, len_out
+
+def generate_array (in_file, out_file, num):
   with open (in_file, "rb") as f:
-       out.write ("//\n// Generated from '%s'\n//\n" % in_file)
-       out.write ("static const unsigned char file%d[] = {\n" % num)
-       data = f.read (-1)
-       for n in range(0, len(data)):
+       out_file.write ("//\n// Generated from '%s'\n//\n" % in_file)
+       out_file.write ("static const unsigned char file%d[] = {\n" % num)
+       data_in  = f.read (-1)
+       data_out = data_in
+       len_in   = len(data_in)
+       len_out  = len_in
+       for n in range(0, len_in):
            if PY2:
-              out.write (" 0x%02X," % ord(data[n]))
+              out.write (" 0x%02X," % ord(data_out[n]))
            else:
-              out.write (" 0x%02X," % data[n])
+              out_file.write (" 0x%02X," % data_out[n])
            if (n + 1) % 16 == 0:
-              out.write ("\n")
-       out.write (" 0x00\n};\n\n")
+              out_file.write ("\n")
+       out_file.write (" 0x00\n};\n\n")
+  return len_in, len_out
 
 def write_packed_files_array (out):
   bytes = 0
@@ -180,6 +244,7 @@ def show_help (error=None):
   print ("""Usage: %s [options] <file-spec>
   -h, --help:         Show this help.
   -c, --case:         Be case-sensitive.
+  -m, --minify:       Compress the .js/.css/.html files first (not for Python2).
   -o, --outfile:      File to generate.
   -r, --recursive:    Walk the sub-directies recursively.
   -s, --strip X:      Strip 'X' from paths.
@@ -189,13 +254,16 @@ def show_help (error=None):
 
 def parse_cmdline():
   parser = argparse.ArgumentParser (add_help = False)
-  parser.add_argument ("-h", "--help",      dest = "help", action = "store_true")
-  parser.add_argument ("-c", "--case",      dest = "casesensitive", action = "store_true")
-  parser.add_argument ("-o", "--outfile",   dest = "outfile", type = str)
-  parser.add_argument ("-r", "--recursive", dest = "recursive", action = "store_true")
-  parser.add_argument ("-s", "--strip",     nargs = "?")
-  parser.add_argument ("-v", "--verbose",   dest = "verbose", action = "count", default = 0)
+  parser.add_argument ("-h", "--help",        dest = "help", action = "store_true")
+  parser.add_argument ("-c", "--case",        dest = "casesensitive", action = "store_true")
+  parser.add_argument ("-m", "--minify",      dest = "minify", action = "store_true")
+  parser.add_argument (      "--no-comments", dest = "nocomments", action = "store_true", default = False)
+  parser.add_argument ("-o", "--outfile",     dest = "outfile", type = str)
+  parser.add_argument ("-r", "--recursive",   dest = "recursive", action = "store_true")
+  parser.add_argument ("-s", "--strip",       nargs = "?")
+  parser.add_argument ("-v", "--verbose",     dest = "verbose", action = "count", default = 0)
   parser.add_argument ("spec", nargs = argparse.REMAINDER)
+
   return parser.parse_args()
 
 opt = parse_cmdline()
@@ -208,12 +276,22 @@ if not opt.outfile:
 if not opt.spec:
    show_help ("Missing 'spec'")
 
+if PY2:
+   have_minify = False
+
+if opt.minify and not have_minify:
+   show_help ("Option '--minify' not available")
+
 opt.spec = opt.spec[0].replace ("\\", "/")
 if opt.spec[-1] in [ "\\", "/" ]:
    opt.spec += "*"
 
 if not os.path.dirname(opt.spec):  # A '*.xx' -> './*.xx'
    opt.spec = "./" + opt.spec
+
+dirname = os.path.dirname(opt.spec)
+if not os.path.exists(dirname):
+   show_help ("Directory '%s' not found" % dirname)
 
 trace (1, "spec: '%s'" % opt.spec)
 
@@ -226,8 +304,32 @@ out = open (opt.outfile, "w+")
 
 out.write (C_TOP % (time.ctime(), sys.executable, __file__))
 
+minifiers = {}
+minifiers [".css"]  = generate_array_css
+minifiers [".js"]   = generate_array_js
+minifiers [".html"] = generate_array_html
+minifiers [".*"]    = generate_array
+
 for n, f in enumerate(files_dict):
-    generate_array (f, n, out)
+    sz = fmt_number (files_dict[f]["fsize"])
+    minified = (f.endswith(".min.css") or f.endswith(".js.css"))
+    if minified:
+       trace (1, "Not minifying '%s'" % f)
+    if minified or not opt.minify:
+       trace (1, "%10s: Generating C-array for '%s'" % (sz, f))
+       len_in, len_out = generate_array (f, out, n)
+
+    else:
+       ext = f [f.rfind("."):]
+       if ext in minifiers:
+          trace (1, "%10s: Generating minified C-array for '%s'" % (sz, f))
+          len_in, len_out = minifiers [ext] (f, out, n)
+       else:
+          trace (1, "%10s: Generating C-array for '%s'" % (sz, f))
+          len_in, len_out = minifiers [".*"] (f, out, n)
+
+    total_in_bytes  += len_in
+    total_out_bytes += len_out
 
 out.write (C_ARRAY)
 write_packed_files_array (out)
@@ -235,3 +337,6 @@ write_packed_files_array (out)
 out.write (C_BOTTOM % opt.casesensitive)
 out.close()
 
+if opt.minify:
+  savings = 100 - 100 * total_out_bytes / total_in_bytes
+  trace (1, "The '--minify' option gave %d%% total savings." % savings)
