27cda9bdb3d52380a8669fd15b5e4a9af8e987f4
Gisle Vanem <gvanem@yahoo.no>
2023-03-24 16:25:54 +0100

    Simplify script.

--- tools/gen_magnitude_lut.py
+++ tools/gen_magnitude_lut.py
@@ -6,18 +6,14 @@
 #
 import math
 
-lut_table = [ 0 ] * 129*129
-print ("static uint16_t py_gen_magnitude_lut [%d] = {" % len(lut_table))
+width = 5
+print ("static uint16_t py_gen_magnitude_lut [129*129] = {")
 
-num = 0
-width = 8
 for I in range(0, 129):
+  print ("\n     // I: %d\n%*s" % (I, width, ""), end="")
   for Q in range(0, 129):
-      v = round (360 * math.hypot (I, Q))
-      lut_table [I*129 + Q] = v
-      print ("%*d, " % (width, v), end="")
-      width = 5
-      num += 1
-      if (num % 10) == 0:
-         print ("\n   ", end="")
+      val = round (360 * math.hypot (I, Q))
+      print ("%*d, " % (width, val), end="")
+      if (Q+1) % 17 == 0:
+         print ("\n%*s" % (width, ""), end="")
 print ("\n};")
