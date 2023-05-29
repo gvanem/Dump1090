f88647f14c459335683d41279cf798959c22a847
Gisle Vanem <gvanem@yahoo.no>
2023-05-29 06:52:22 +0200

    For a future 'packed_test.exe' program. To compare the output of the non-minified and minified '$(OBJ_DIR)/packed_webfs*.c' files.

---
+++ tools/packed_webfs_main.c
@@ -0,0 +1,16 @@
+/**
+ * For 'packed_test.exe', but not yet.
+ */
+#include "mongoose.h"
+
+const char *mg_unlist_1 (size_t i);
+const char *mg_unpack_1 (const char *name, size_t *size, time_t *mtime);
+
+const char *mg_unlist_2 (size_t i);
+const char *mg_unpack_2 (const char *name, size_t *size, time_t *mtime);
+
+int main (int argc, char **argv)
+{
+  (void) argc;
+  (void) argv;
+}
