ac124e7a435ad916cc60935b226fe2eb3a149c41
gvanem <gvanem@yahoo.no>
2023-10-23 15:12:21 +0200

    Another minor fix.

--- location.h
+++ location.h
@@ -3,15 +3,10 @@
  *
  * Simple async 'Windows Location API' interface.
  */
-#ifndef _LOCATION_H
-#define _LOCATION_H
+#pragma once
 
 #include <stdbool.h>
 
-struct pos_t;
-
 void location_exit (void);
-bool location_poll (struct pos_t *pos);
+bool location_poll (pos_t *pos);
 bool location_get_async (void);
-
-#endif
