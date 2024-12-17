e9b1d42058b30ebdfb2c448a0f8eafb4f02af0af
gvanem <gvanem@yahoo.no>
2024-12-17 15:03:48 +0100

    Dropped the use of '-DUSE_GEN_ROUTES'. Only option is '-DUSE_BIN_FILES'.

--- src/routes.h
+++ src/routes.h
@@ -1,7 +1,7 @@
 /**\file    routes.h
  * \ingroup Main
  *
- * Lookup of generated routes[] data.
+ * Lookup of generated `*route_records` data.
  */
 #pragma once
 
@@ -38,14 +38,4 @@
 
   #pragma pack(pop)
 
-#else
-  typedef struct route_record {
-          char  call_sign    [10];  /**< Call-sign for this route (or flight) */
-          char  departure    [10];  /**< ICAO departure airport for this route */
-          char  destination  [10];  /**< Final ICAO destination airport for this route */
-          char  stop_over [5][10];  /**< 5 possible stop-over airports. Or "" for none */
-        } route_record;
-
-  extern const route_record route_records[];
-  extern size_t             route_records_num;
 #endif
