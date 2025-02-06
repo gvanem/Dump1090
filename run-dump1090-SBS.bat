bb9f44859e96118cd1ba867fa6f64470c307aee7
gvanem <gvanem@yahoo.no>
2025-02-06 12:09:37 +0100

    Allow the .bat to run from another directory.

--- run-dump1090-SBS.bat
+++ run-dump1090-SBS.bat
@@ -3,6 +3,7 @@ setlocal
 set RTLSDR_TRACE=
 
 if %_cmdproc. == 4NT. .or. %_cmdproc. == TCC. on break quit
-start py.exe -3 tools/SBS_client.py --wait 5 --host localhost --port 30003 SBS
-dump1090.exe --net --interactive
+start py.exe -3 %~dp0\tools\SBS_client.py --wait 5 --host localhost --port 30003 SBS
+%~dp0\dump1090.exe --net --interactive
+
 
